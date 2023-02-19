fa6464a27b9ec0b2ccec4ef4d8fb4768ccde9ad9
rivalnofirm <rivalno@detik.com>
2023-02-19 22:20:49 +0700

    update template project

--- product-service/models/brand.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Brand struct {
-	ID        int64  `json:"id"`
-	Name      string `json:"brand_name"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
-}
--- product-service/models/product.go
+++ product-service/models/product.go
@@ -1,12 +1,1 @@
 package models
-
-import "time"
-
-type Product struct {
-	ID        int64     `json:"id"`
-	Name      string    `json:"product_name" validate:"required"`
-	Price     float32   `json:"price" validate:"required"`
-	Brand     Brand     `json:"author"`
-	UpdatedAt time.Time `json:"updated_at"`
-	CreatedAt time.Time `json:"created_at"`
-}
